ddb12fccb21d1982fdfbbccba451d56314920e75
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-10-15 22:07:14 +0300

    Add files via upload

---
+++ module_10_4.py
@@ -0,0 +1,78 @@
+import time
+from threading import Thread
+import queue
+import random
+
+
+
+guests_names = [
+    'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman', 'Vitoria', 'Nikita',
+    'Galina', 'Pavel', 'Ilya','Alexandra']
+
+
+class Table:
+    def __init__(self, number: int):
+        self.number = number
+        self.guest = None
+
+class Guest(Thread):
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        time.sleep(random.randint(3, 10))
+
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = queue.Queue()
+        self.tables = list(tables)
+
+
+    def guest_arrival(self, *guest):
+        for guest in guests:
+            table = self.free_table()
+            if table:
+                table.guest = guest
+                guest.start()
+                print(f'{guest.name} сел(-а) за стол номер {table.number}')
+            else:
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
+
+
+    def discuss_guests(self):
+        while not self.queue.empty() or self.busy_table():
+            for table in self.tables:
+                if table.guest and not table.guest.is_alive():
+                    print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
+                    print(f'Стол номер {table.number} свободен')
+                    table.guest = None
+
+                if not self.queue.empty() and table.guest is None:
+                    next_guest = self.queue.get()
+                    table.guest = next_guest
+                    next_guest.start()
+                    print(f'{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+
+    def free_table(self):
+        for table in self.tables:
+            if table.guest is None:
+                return table
+        return None
+
+    def busy_table(self):
+        return any(table.guest is not None for table in self.tables)
+
+
+
+guests = [Guest(name) for name in guests_names]
+tables = [Table(number) for number in range(1, 8)]
+cafe = Cafe(*tables)
+cafe.guest_arrival(*guests)
+cafe.discuss_guests()
+
+
+
+
+
